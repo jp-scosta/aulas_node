a5787aae41911d0ea06cf0334c96689622e5147c
jp-scosta <joao.pedro.scosta2006@gmail.com>
2023-10-02 19:39:44 -0300

    recapitulação de tender html

---
+++ 2_EXPRESS/5_recap/index.js
@@ -0,0 +1,17 @@
+const express = require("express") 
+const path = require("path")
+
+const app = express()
+
+const caminho = path.join(__dirname, "templates")
+
+app.get('/', (requisicao, resposta) => {
+    resposta.sendFile(`${caminho}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na portinha 3000")
+})
+
+
+// http://localhost:3000
